a8fa2192565e8c4a9e1b6a7b843857c9a5c8e72e
Pasha Nikolauchyk <pasha.nikolauchyk044@ukr.net>
2024-11-02 11:28:07 +0200

    mod11

--- src/main.js
+++ src/main.js
@@ -1,12 +1,50 @@
-// У файлі main.js
-import validator from 'validator';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-console.log(
-  'Is mango@mail.com a valid email?: ',
-  validator.isEmail('mango@mail.com')
-); // Is mango@mail.com a valid email?: true
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(
-  'Is Mangodogmail.com a valid email?: ',
-  validator.isEmail('Mangodogmail.com')
-); // Is mango@mail.com a valid email?: false
+import { fetchData } from './js/pixabay-api';
+import { createMarkup } from './js/render-functions';
+
+const form = document.querySelector('.form-send-js');
+const list = document.querySelector('.list-js');
+const lightbox = new SimpleLightbox('.gallery-item a'); // Ініціалізація SimpleLightbox
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const searchQuery = event.target.elements.search.value.trim();
+
+  if (!searchQuery) {
+    iziToast.error({ title: 'Error', message: 'Please enter a search query.' });
+    return;
+  }
+
+  list.innerHTML = '';
+
+  fetchData(searchQuery)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.info({
+          title: 'No results',
+          position: 'topRight',
+          backgroundColor: '#ff4d4f',
+
+          message:
+            'Sorry, there are no images matching your search query. Please, try again!',
+        });
+      } else {
+        list.innerHTML = createMarkup(data.hits);
+        lightbox.refresh();
+      }
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: 'Failed to fetch images. Try again.',
+      });
+    });
+}
